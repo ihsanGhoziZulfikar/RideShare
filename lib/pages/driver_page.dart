ccb61e35b79ac7d597b0674df6f76556d827a747
adem299 <mulyanaade30@gmail.com>
2024-06-28 15:53:01 +0700

    Update Driver Guest

--- lib/pages/driver_page.dart
+++ lib/pages/driver_page.dart
@@ -2,11 +2,13 @@ import 'dart:async';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:geocoding/geocoding.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:location/location.dart';
+import 'package:location/location.dart' as loc;
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ride_share/constant.dart';
+import 'package:ride_share/pages/chat_page.dart';
 
 class DriverPage extends StatefulWidget {
   const DriverPage({super.key});
@@ -17,7 +19,7 @@ class DriverPage extends StatefulWidget {
 
 class _DriverPageState extends State<DriverPage> {
   final Completer<GoogleMapController> _controller = Completer();
-  final Location locationController = Location();
+  final loc.Location locationController = loc.Location();
   late GoogleMapController _mapController;
   StreamSubscription<QuerySnapshot>? _requestSubscription;
 
@@ -29,8 +31,15 @@ class _DriverPageState extends State<DriverPage> {
 
   LatLng? currentPosition;
   Map<PolylineId, Polyline> polylines = {};
-
   final Set<Marker> _markers = {};
+
+  String _selectedMarkerTitle = '';
+  String _selectedMarkerSubtitle = '';
+  String? _selectedMarkerEmail;
+  String? _selectedMarkerId;
+  bool _isMarkerSelected = false;
+
+  String _passengerName = '';
 
   @override
   void initState() {
@@ -71,7 +80,7 @@ class _DriverPageState extends State<DriverPage> {
 
   Future<void> _requestLocationPermission() async {
     final status = await locationController.requestPermission();
-    if (status == PermissionStatus.granted) {
+    if (status == loc.PermissionStatus.granted) {
       setState(() {
         _locationPermissionGranted = true;
       });
@@ -88,7 +97,7 @@ class _DriverPageState extends State<DriverPage> {
       for (var docChange in querySnapshot.docChanges) {
         if (docChange.type == DocumentChangeType.added) {
           var request = docChange.doc.data() as Map<String, dynamic>;
-          request['id'] = docChange.doc.id; // Simpan ID dokumen dalam request
+          request['id'] = docChange.doc.id;
 
           print('New request from passenger: ${request['passengerId']}');
           _showNewRequestDialog(request);
@@ -97,14 +106,27 @@ class _DriverPageState extends State<DriverPage> {
     });
   }
 
+  Future<String> getCityName(double latitude, double longitude) async {
+    try {
+      List<Placemark> placemarks = await placemarkFromCoordinates(latitude, longitude);
+      if (placemarks.isNotEmpty) {
+        return placemarks.first.locality ?? 'Unknown';
+      }
+    } catch (e) {
+      print('Error occurred while fetching city name: $e');
+    }
+    return 'Unknown';
+  }
+
   void _showNewRequestDialog(Map<String, dynamic> request) {
-    FirebaseFirestore.instance.collection('Users').doc(request['passengerId']).get().then((userData) {
+    FirebaseFirestore.instance.collection('Users').doc(request['passengerId']).get().then((userData) async {
       if (userData.exists) {
         String passengerUsername = userData['username'];
         double passengerLat = double.tryParse(userData['lat'].toString()) ?? 0.0;
         double passengerLng = double.tryParse(userData['lng'].toString()) ?? 0.0;
+        String cityName = await getCityName(passengerLat, passengerLng);
 
-        print('Passenger data: Username: $passengerUsername, Lat: $passengerLat, Lng: $passengerLng');
+        print('Passenger data: Username: $passengerUsername');
 
         showDialog(
           context: context,
@@ -112,7 +134,7 @@ class _DriverPageState extends State<DriverPage> {
             return AlertDialog(
               title: Text('New Ride Request'),
               content: SingleChildScrollView(
-                child: Text('You have a new ride request from passenger $passengerUsername. Location: ($passengerLat, $passengerLng)'),
+                child: Text('Kamu memiliki permintaan Rideshare baru dari penumpang $passengerUsername. Location: $cityName'),
               ),
               actions: [
                 TextButton(
@@ -121,14 +143,12 @@ class _DriverPageState extends State<DriverPage> {
                     Navigator.of(context).pop();
 
                     try {
-                      // Cek apakah dokumen ada sebelum memperbarui
                       final docSnapshot = await FirebaseFirestore.instance.collection('Requests').doc(request['id']).get();
                       if (docSnapshot.exists) {
                         await FirebaseFirestore.instance.collection('Requests').doc(request['id']).update({
                           'status': 'seen'
                         });
 
-                        // Add guest marker
                         setState(() {
                           _markers.add(
                             Marker(
@@ -137,6 +157,17 @@ class _DriverPageState extends State<DriverPage> {
                               infoWindow: InfoWindow(
                                 title: "Guest: $passengerUsername",
                               ),
+                              icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
+                              onTap: () {
+                                setState(() {
+                                  _selectedMarkerTitle = passengerUsername;
+                                  _selectedMarkerSubtitle = userData['destination'] ?? 'Unknown';
+                                  _selectedMarkerEmail = userData['email'];
+                                  _selectedMarkerId = request['passengerId'];
+                                  _isMarkerSelected = true;
+                                  _passengerName = userData['username'];
+                                });
+                              },
                             ),
                           );
                         });
@@ -167,38 +198,187 @@ class _DriverPageState extends State<DriverPage> {
       appBar: AppBar(
         title: const Text('Track nebeng'),
       ),
-      body: currentPosition == null
-          ? const Center(
-              child: CircularProgressIndicator(),
-            )
-          : GoogleMap(
-              onMapCreated: _onMapCreated,
-              initialCameraPosition: const CameraPosition(
-                target: sourceLocation,
-                zoom: 14.5,
-              ),
-              markers: _markers.union({
-                Marker(
-                  markerId: const MarkerId("current"),
-                  position: currentPosition!,
+      body: Stack(
+        children: [
+          currentPosition == null
+              ? const Center(child: CircularProgressIndicator())
+              : GoogleMap(
+                  onMapCreated: _onMapCreated,
+                  initialCameraPosition: const CameraPosition(
+                    target: sourceLocation,
+                    zoom: 14.5,
+                  ),
+                  markers: _markers.union({
+                    if (currentPosition != null)
+                      Marker(
+                        markerId: const MarkerId("current"),
+                        position: currentPosition!,
+                      ),
+                  }),
+                  polylines: Set<Polyline>.of(polylines.values),
                 ),
-                // Marker(
-                //   markerId: const MarkerId("source"),
-                //   position: sourceLocation,
-                // ),
-                // Marker(
-                //   markerId: const MarkerId("destination"),
-                //   position: destination,
-                // ),
-              }),
-              polylines: Set<Polyline>.of(polylines.values),
+          Positioned(
+            right: 0,
+            left: 0,
+            bottom: 0,
+            child: AnimatedContainer(
+              duration: Duration(milliseconds: 150),
+              curve: Curves.easeInOut,
+              width: MediaQuery.of(context).size.width,
+              height: _isMarkerSelected ? MediaQuery.of(context).size.height / 3.2 : 0,
+              alignment: Alignment.bottomCenter,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(30),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 20,
+                    offset: Offset.zero,
+                    color: Colors.grey.withOpacity(0.5),
+                  ),
+                ],
+              ),
+              child: _isMarkerSelected
+                  ? Column(
+                      children: [
+                        ListTile(
+                          leading: CircleAvatar(
+                            backgroundImage: NetworkImage('https://images.unsplash.com/photo-1599566150163-29194dcaad36?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
+                          ),
+                          title: Text(
+                            _selectedMarkerTitle,
+                            style: TextStyle(fontFamily: 'Kantumruy', fontWeight: FontWeight.bold, fontSize: 18),
+                          ),
+                          subtitle: Row(
+                            children: [
+                              Icon(Icons.directions),
+                              SizedBox(width: 4),
+                              Text(
+                                _selectedMarkerSubtitle,
+                                style: TextStyle(
+                                  fontFamily: 'Kantumruy',
+                                ),
+                              ),
+                            ],
+                          ),
+                          trailing: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              IconButton(
+                                icon: Icon(Icons.message),
+                                onPressed: () {
+                                  if (_selectedMarkerEmail != null && _selectedMarkerId != null) {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => ChatPage(
+                                          receiverEmail: _selectedMarkerEmail!,
+                                          receiverId: _selectedMarkerId!,
+                                        ),
+                                      ),
+                                    );
+                                  }
+                                },
+                              ),
+                              IconButton(
+                                icon: Icon(Icons.call),
+                                onPressed: () {},
+                              ),
+                            ],
+                          ),
+                        ),
+                        Divider(),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(4.0),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black26,
+                                  blurRadius: 1,
+                                  spreadRadius: 1,
+                                  offset: Offset(0, 1),
+                                ),
+                              ],
+                            ),
+                            child: ListTile(
+                              title: Text(
+                                'Email',
+                                style: TextStyle(
+                                  fontFamily: 'Kantumruy',
+                                ),
+                              ),
+                              subtitle: Text(
+                                _selectedMarkerEmail ?? '',
+                                style: TextStyle(
+                                  fontFamily: 'Kantumruy',
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
+                          child: GestureDetector(
+                            onTap: () {
+                              showDialog(
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return AlertDialog(
+                                    title: Text('Notification'),
+                                    content: Text('Sekarang $_passengerName menjadi penumpang mu.'),
+                                    actions: [
+                                      TextButton(
+                                        style: ButtonStyle(
+                                          backgroundColor: WidgetStatePropertyAll(Colors.white),
+                                        ),
+                                        child: Text('OK'),
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                          _isMarkerSelected = false;
+                                        },
+                                      ),
+                                    ],
+                                  );
+                                },
+                              );
+                            },
+                            child: Container(
+                              width: double.infinity,
+                              decoration: BoxDecoration(
+                                color: Color.fromRGBO(0, 119, 182, 0.25),
+                                borderRadius: BorderRadius.circular(5),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: Center(
+                                  child: Text(
+                                    'Terima Kuy',
+                                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Color(0xfff0077B6)),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
+                  : SizedBox.shrink(),
             ),
+          ),
+        ],
+      ),
     );
   }
 
   Future<void> fetchLocationUpdates() async {
     bool serviceEnabled;
-    PermissionStatus permissionGranted;
+    loc.PermissionStatus permissionGranted;
 
     serviceEnabled = await locationController.serviceEnabled();
     if (!serviceEnabled) {
@@ -209,24 +389,39 @@ class _DriverPageState extends State<DriverPage> {
     }
 
     permissionGranted = await locationController.hasPermission();
-    if (permissionGranted == PermissionStatus.denied) {
+    if (permissionGranted == loc.PermissionStatus.denied) {
       permissionGranted = await locationController.requestPermission();
-      if (permissionGranted != PermissionStatus.granted) {
+      if (permissionGranted != loc.PermissionStatus.granted) {
         return;
       }
     }
 
-    locationController.onLocationChanged.listen((LocationData currentLocation) {
-      if (currentLocation.latitude != null && currentLocation.longitude != null) {
-        if (mounted) {
-          setState(() {
-            currentPosition = LatLng(
-              currentLocation.latitude!,
-              currentLocation.longitude!,
-            );
-          });
+    locationController.onLocationChanged.listen(
+      (loc.LocationData currentLocation) async {
+        if (currentLocation.latitude != null && currentLocation.longitude != null) {
+          if (mounted) {
+            setState(() {
+              currentPosition = LatLng(
+                currentLocation.latitude!,
+                currentLocation.longitude!,
+              );
+            });
+          }
+
+          final currentUser = FirebaseAuth.instance.currentUser;
+          if (currentUser != null) {
+            try {
+              await FirebaseFirestore.instance.collection('Users').doc(currentUser.uid).update({
+                'lat': currentLocation.latitude,
+                'lng': currentLocation.longitude,
+              });
+              print('Location updated in Firestore: (${currentLocation.latitude}, ${currentLocation.longitude})');
+            } catch (e) {
+              print('Error updating location in Firestore: $e');
+            }
+          }
         }
-      }
-    });
+      },
+    );
   }
 }
