37aa52dc60762e8478feb0506b955199a2bf6558
bahtiaryusuf10 <mhmmdbachtiar10@gmail.com>
2024-06-28 11:19:13 +0700

    fix navbar & remake chat page

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:iconify_flutter/iconify_flutter.dart';
 import 'package:ride_share/components/chat_bubble.dart';
 import 'package:ride_share/components/my_textfield.dart';
 import 'package:ride_share/services/auth/auth_service.dart';
@@ -89,7 +90,7 @@ class ChatPage extends StatelessWidget {
 
   Widget _buildUserInput() {
     return Padding(
-      padding: const EdgeInsets.all(8.0),
+      padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 2.0),
       child: Row(
         children: [
           Expanded(
@@ -102,7 +103,10 @@ class ChatPage extends StatelessWidget {
           ),
           IconButton(
             onPressed: sendMessage,
-            icon: Icon(Icons.arrow_circle_right_rounded),
+            icon: Iconify(
+              '<svg xmlns="http://www.w3.org/2000/svg" width="1rem" height="1rem" viewBox="0 0 24 24"><defs><path id="letsIconsSendDuotone0" fill="#0077B6" d="m7.692 11.897l1.41.47c.932.31 1.397.466 1.731.8c.334.334.49.8.8 1.73l.47 1.41c.784 2.354 1.176 3.53 1.897 3.53c.72 0 1.113-1.176 1.897-3.53l2.838-8.512c.552-1.656.828-2.484.391-2.921c-.437-.437-1.265-.161-2.92.39l-8.515 2.84C5.34 8.887 4.162 9.279 4.162 10c0 .72 1.177 1.113 3.53 1.897"/></defs><use href="#letsIconsSendDuotone0" fill-opacity="0.25"/><use href="#letsIconsSendDuotone0" fill-opacity="0.25"/><path fill="#0077B6" d="m9.526 13.842l-2.062-.687a1 1 0 0 0-.87.116l-1.09.726a.8.8 0 0 0 .25 1.442l1.955.488a.5.5 0 0 1 .364.364l.488 1.955a.8.8 0 0 0 1.442.25l.726-1.09a1 1 0 0 0 .116-.87l-.687-2.062a1 1 0 0 0-.632-.632"/></svg>',
+              size: 45,
+            ),
           ),
         ],
       ),
