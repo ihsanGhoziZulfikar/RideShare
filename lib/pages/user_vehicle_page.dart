c20825cd479948bbb4f05f7feabacdee7b79e8a0
bahtiaryusuf10 <mhmmdbachtiar10@gmail.com>
2024-06-28 17:11:49 +0700

    remake vehicle list page

--- lib/pages/user_vehicle_page.dart
+++ lib/pages/user_vehicle_page.dart
@@ -39,11 +39,40 @@ class _UserVehiclesPageState extends State<UserVehiclesPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('My Vehicles'),
-      ),
+      backgroundColor: Colors.white,
       body: Column(
         children: [
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 20.0,
+              left: 10,
+              bottom: 15,
+            ),
+            child: Row(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: Icon(
+                    Icons.arrow_back_rounded,
+                    size: 30,
+                  ),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  "My Vehicles",
+                  style: TextStyle(
+                    fontFamily: 'Kantumruy',
+                    fontSize: 18,
+                    color: Colors.black,
+                  ),
+                ),
+              ],
+            ),
+          ),
           Expanded(
             child: FutureBuilder<List<Map<String, dynamic>>>(
               future: _fetchUserVehicles(),
@@ -58,34 +87,98 @@ class _UserVehiclesPageState extends State<UserVehiclesPage> {
 
                 List<Map<String, dynamic>> vehicles = snapshot.data!;
 
-                return ListView.builder(
-                  itemCount: vehicles.length,
-                  itemBuilder: (context, index) {
-                    Map<String, dynamic> vehicle = vehicles[index];
-                    return ListTile(
-                      title: Text(vehicle['name']),
-                      subtitle: Text(
-                          'License: ${vehicle['license']} \nType: ${vehicle['type']}'),
-                    );
-                  },
+                return Padding(
+                  padding: const EdgeInsets.only(left: 30.0, right: 10),
+                  child: ListView.builder(
+                    itemCount: vehicles.length,
+                    itemBuilder: (context, index) {
+                      Map<String, dynamic> vehicle = vehicles[index];
+                      return Card(
+                        surfaceTintColor: Color(0xFF00B4D8),
+                        margin: EdgeInsets.all(10),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Image.asset(
+                                vehicle['type'].contains('car')
+                                    ? 'assets/images/mobil.jpg'
+                                    : 'assets/images/motor.jpg',
+                                height: 100,
+                                width: 100,
+                                fit: BoxFit.cover,
+                              ),
+                              SizedBox(
+                                width: 20,
+                              ),
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    SizedBox(height: 5),
+                                    Text(
+                                      vehicle['name'],
+                                      style: TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.grey[800],
+                                      ),
+                                    ),
+                                    SizedBox(height: 5),
+                                    Text(
+                                      'License: ${vehicle['license']} \nType: ${vehicle['type']}',
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                        color: Colors.grey[600],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Align(
+                                alignment: Alignment.topRight,
+                                child: PopupMenuButton<String>(
+                                  onSelected: (String value) {
+                                    if (value == 'Ubah') {
+                                      SnackBar(
+                                        content: Text("mengubah vehicle"),
+                                      );
+                                    }
+                                  },
+                                  itemBuilder: (BuildContext context) {
+                                    return {'Ubah'}.map((String choice) {
+                                      return PopupMenuItem<String>(
+                                        value: choice,
+                                        child: Text(choice),
+                                      );
+                                    }).toList();
+                                  },
+                                  icon: Icon(Icons.more_vert),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  ),
                 );
               },
             ),
           ),
-          Container(
-            padding: EdgeInsets.all(8.0),
-            color: Colors.grey,
-            child: IconButton(
-              onPressed: (() {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => AddVehiclePage()),
-                );
-              }),
-              icon: Icon(Icons.add),
-            ),
-          ),
         ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => AddVehiclePage()),
+          );
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        shape: CircleBorder(),
       ),
     );
   }
